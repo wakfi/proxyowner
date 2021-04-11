ffa6cb019a8e35280b1880c8f7f37ff8aaf1c58b
Walker Gray <55608093+wakfi@users.noreply.github.com>
2021-04-10 19:50:07 -0700

    Fix zero padding not padding/way overpadding

--- src/util/padZeros.js
+++ src/util/padZeros.js
@@ -1,7 +1,8 @@
 function padZeros(number, zeros) {
-	zeros = zeros || 0;
-	const padded = (Array.from({length:zeros}, ()=>'0')).join('');
-	return padded.slice(-(number.length))+number;
+	number = ''+number;
+	zeros = zeros || 2;
+	const padded = (Array.from({length:zeros},()=>'0')).join('');
+	return padded.slice(0, padded.length-number.length)+number;
 }
 
 module.exports = padZeros;
